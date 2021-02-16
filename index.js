0eea9377942f27d35714963bab2bb230f1cf4302
Joao Rangel <joao.vran@gmail.com>
2021-02-16 09:29:47 -0300

    remove last '/' from request path

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 import { createServer } from 'http';
+import { parse } from 'url';
 
 const server = createServer((request, response) => {
+  const parsedUrl = parse(request.url, true);
+
+  const path = parsedUrl.pathname;
+  let trimmedPath = path;
+
+  const lastCharacterIndex = path.length - 1
+  if (path.slice(lastCharacterIndex) === '/') {
+    trimmedPath = path.slice(0, lastCharacterIndex);
+  }
+
   response.end('Hello World');
+
+  console.log(`Request received on path: ${trimmedPath}`);
 });
 
 const PORT = 3000;
