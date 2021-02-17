b7eede8ea18a1e35899531aff46396b2645c27f1
Joao Rangel <joao.vran@gmail.com>
2021-02-17 12:48:20 -0300

    refactor trimmedPath

--- index.js
+++ index.js
@@ -6,12 +6,10 @@ const server = createServer((request, response) => {
   const parsedUrl = parse(request.url, true);
 
   const path = parsedUrl.pathname;
-  let trimmedPath = path;
 
-  const lastCharacterIndex = path.length - 1;
-  if (path.slice(lastCharacterIndex) === '/') {
-    trimmedPath = path.slice(0, lastCharacterIndex);
-  };
+  const splitPath = path.split('/');
+  const trimmedSplitPath = splitPath.filter((term) => term !== '');
+  const trimmedPath = trimmedSplitPath.join('/');
 
   const method = request.method.toUpperCase();
 
